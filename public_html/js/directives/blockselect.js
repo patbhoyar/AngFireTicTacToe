15cbf9962bf501deb265d285e5cb71383f970349
patb <patbhoyar@gmail.com>
2015-03-16 00:23:41 +0530

    small typo fixed

--- public_html/js/directives/blockselect.js
+++ public_html/js/directives/blockselect.js
@@ -6,7 +6,7 @@ app
                 var indices = attrs.selectBlock.split(",");
                 var newIndices = $scope.gameData.board;
                 
-                if(indices[0] === '0' && indices[0] === '0'){ newIndices[0] = $scope.gameData.playerIcon; }
+                if(indices[0] === '0' && indices[1] === '0'){ newIndices[0] = $scope.gameData.playerIcon; }
                 else if(indices[0] === '0' && indices[1] === '1'){ newIndices[1] = $scope.gameData.playerIcon; }
                 else if(indices[0] === '0' && indices[1] === '2'){ newIndices[2] = $scope.gameData.playerIcon; }
                 else if(indices[0] === '1' && indices[1] === '0'){ newIndices[3] = $scope.gameData.playerIcon; }
@@ -16,8 +16,8 @@ app
                 else if(indices[0] === '2' && indices[1] === '1'){ newIndices[7] = $scope.gameData.playerIcon; }
                 else if(indices[0] === '2' && indices[1] === '2'){ newIndices[8] = $scope.gameData.playerIcon; }
                 
-                 $(element).text($scope.gameData.playerIcon);
-                 $(element).attr('disabled','disabled');
+                $(element).text($scope.gameData.playerIcon);
+                $(element).attr('disabled','disabled');
                 
                 $scope.gameRef.update({
                     playerTurn:$scope.changePlayer(),
